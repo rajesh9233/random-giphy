637e50163a841ea7febb376bebb5102d58566510
Rajesh <rajeshmurugesan228@gmail.com>
2021-08-01 17:32:24 +0530

    latest changes

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+// "use strict"
 let randomWordURi = "https://random-word-api.herokuapp.com/word?number=1"
 let giphyURi = "https://api.giphy.com/v1/gifs/search?api_key=Cc0cDk9PZZMUNjMgvr0MssB098TJR3kN&limit=25&offset=0&rating=g&lang=en&q="
 let wordId = document.getElementById('word');
 let gifRow = document.getElementById('gif-row');
 
-async function getGIF(event){
+async function getGIF(){
     try {
-        // gifRow.remove();
-        console.log(event.value);
         let resp1 = await fetch(randomWordURi);
         let data = await resp1.json();
         let word = data[0];
         console.log(word)
-        let resp2 = await fetch(giphyURi + event.value);
+        let resp2 = await fetch(giphyURi + word);
         let data2 = await resp2.json();
         const gif = data2.data
         console.log(data2,gif);
-        wordId.innerHTML = event.value;
+        wordId.innerHTML = word;
         console.log(gif);
         if(gif.length === 0){
-            wordId.innerHTML = 'No GiF found for the word' + event.value; 
+            wordId.innerHTML = 'No GiF found for the word ' + word; 
         }
         gif.forEach(item=>{
             const col = document.createElement('div');
@@ -33,4 +32,4 @@ async function getGIF(event){
     }
 }
 
-// getGIF();
+getGIF();
